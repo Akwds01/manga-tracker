cbb4b8f88315fdada90cb4e06c10f16cbcbffc5f
AKWILA <74317677+Akwds01@users.noreply.github.com>
2026-07-07 15:49:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import cloudscraper
 import urllib3
 
 # Mematikan notifikasi peringatan SSL Insecure di log Heroku
-urllib3.disable_warnings(urllib3.errors.InsecureRequestWarning)
+urllib3.disable_warnings()
 
 # 1. Konfigurasi Sistem (Diambil dari Heroku Config Vars)
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
